810b52cfdbd50173530d2165440f09389f40f51a
Mohit Arora <32641304+mohitarora3@users.noreply.github.com>
2018-03-23 13:36:19 +0530

    queue operations using stack(using stl)

---
+++ queue_using_stack.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<stack>
+#include<conio.h>
+#define max_size 10
+using namespace std;
+class queue
+{
+	stack s1,s2;
+	int curr_size;
+	public:
+		void enque(int);
+		int deque();
+		bool isempty();
+		int size()
+		{
+			return curr_size;
+		}
+};
+
+
+void queue::enque(int ele)
+{
+	s.push(ele);
+}
+
+int queue::deque()
+{
+	while(!s1.empty())
+	{
+		s2.push(s1.top());
+		s1.pop();
+	}
+	stack s;
+	int x=s1.top();
+	s1.pop();
+	s=s1;
+	s1=s2;
+	s2=s;
+	return x;
+}
+bool queue::isempty()
+{
+	return (s1.top()=-1);
+}
+
+int main()
+{
+	int x,ele,ch;
+	char c;
+//	queue q;
+	///*
+//	q.enque(1);
+//	q.enque(2);
+//	q.enque(3);
+//	cout<<q.deque();
+//	cout<<q.deque();
+//	cout<<q.deque();
+    queue q;
+	do{
+	cout<<"\n\tQUEUE OPERATIONS USING STACK";
+	cout<<"\n\t1.ENQUE";
+	cout<<"\n\t2.DEQUE";
+	cout<<"\n\tENTER YOUR CHOICE";
+	cin>>ch;
+	switch(ch)
+	{
+		case 1:cout<<"\n\tENTER ELEMENT";
+				cin>>ele;
+				q.enque(ele);
+				break;
+		case 2:x=q.deque();
+				if(x!=-1)
+					cout<<"\n\tPOPPED ELEMENT IS:"<<x;
+				break;
+
+		default:cout<<"\n\tWRONG CHOICE!";
+				break;
+	}
+	cout<<"\n\twant to continue(enter y or Y)";
+	cin>>c;
+}while(c=='y'||c=='Y');
+
+getch();
+}
+
